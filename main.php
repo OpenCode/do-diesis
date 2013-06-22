df1529b97fbb02db3974eb8fd0c93d27b8d283ab
Francesco Apruzzese <cescoap@gmail.com>
2013-06-22 12:59:40 +0200

    [FIX] Really fixed issue #1 :)

--- main.php
+++ main.php
@@ -73,13 +73,23 @@
 	} // if
 
 	// Get the order
-	$order_type = 'date DESC';
+	$order = 'date DESC';
 	if ( $_GET && isset($_GET['order']) ) {
-		$order_type = $_GET['order'];
+		$order = $_GET['order'];
 		}
 
+	// Get the filter
+	/*$filter = 'id';
+	$operator = '>';
+	$value = 0;
+	if ( $_GET && isset($_GET['filter']) ) {
+		$filter = $_GET['filter'];
+		$operator = $_GET['operator'];
+		$value = $_GET['value'];
+		}*/
+
 	// Extract all the main record
-	$records = R::findAll(__MAIN_TABLE__, ' ORDER BY ' . $order_type . ' ');
+	$records = R::findAll(__MAIN_TABLE__, ' ORDER BY ' . $order . ' ');
 
 ?>
 
@@ -133,7 +143,7 @@
 						</div>
 						<div class="controls controls-row">
 							<input class="span2" id="date" name="date" type="text" placeholder="Date" required readonly value="<?php  echo date("d/m/Y"); ?>">
-							<input class="span3" id="group_id" name="group_id" type="text" placeholder="Group" autocomplete="off" required>
+							<input class="span3" id="group_id" name="group_id" type="text" placeholder="Group" autocomplete="off" required onFocus="$('.datepicker').css('display', 'none');">
 							<input class="span2" id="payment_method_id" name="payment_method_id" type="text" placeholder="Payment Method" autocomplete="off" required>
 							<input class="span2" name="in" id="in" type="text" placeholder="In">
 							<input class="span2" name="out" id="out" type="text" placeholder="Out">
@@ -148,8 +158,8 @@
 							echo '<table class="table table-striped table-bordered table-hover table-condensed">
 									<tr>
 										<td></td>
-										<td><b><i class="icon-chevron-'.get_order_icon($order_type, 'description').'"></i> <a href="?order='.invert_order($order_type, 'description').'">DESCRIPTION</a></b></td>
-										<td><b><i class="icon-chevron-'.get_order_icon($order_type, 'date').'"></i> <a href="?order='.invert_order($order_type, 'date').'">DATE</a></b></td>
+										<td><b><i class="icon-chevron-'.get_order_icon($order, 'description').'"></i> <a href="?order='.invert_order($order, 'description').'">DESCRIPTION</a></b></td>
+										<td><b><i class="icon-chevron-'.get_order_icon($order, 'date').'"></i> <a href="?order='.invert_order($order, 'date').'">DATE</a></b></td>
 										<td><b>PARTNER</b></td>
 										<td><b>GROUP</b></td>
 										<td><b>PAYMENT</b></td>
